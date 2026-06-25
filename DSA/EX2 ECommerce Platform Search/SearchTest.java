bb23b0795c3b954d3bde65958431f36e1319ab52
bhavadharani-02-09-06 <230229.cs@rmkec.ac.in>
2026-06-25 22:47:12 +0530

    Convert EX2 ECommerce Platform Search to normal folder

---
+++ DSA/EX2 ECommerce Platform Search/SearchTest.java
@@ -0,0 +1,36 @@
+public class SearchTest {
+
+    public static void main(String[] args) {
+
+        Product[] products = {
+
+                new Product(101, "Laptop", "Electronics"),
+                new Product(102, "Phone", "Electronics"),
+                new Product(103, "Shoes", "Fashion"),
+                new Product(104, "Watch", "Accessories"),
+                new Product(105, "Headphones", "Electronics")
+
+        };
+
+        System.out.println("Linear Search:");
+
+        Product result1 = SearchAlgorithms.linearSearch(products, 104);
+
+        if (result1 != null)
+            System.out.println(result1);
+        else
+            System.out.println("Product Not Found");
+
+
+
+        System.out.println("\nBinary Search:");
+
+        Product result2 = SearchAlgorithms.binarySearch(products, 104);
+
+        if (result2 != null)
+            System.out.println(result2);
+        else
+            System.out.println("Product Not Found");
+
+    }
+}
